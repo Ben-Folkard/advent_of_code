8a1193f3a8897b94fe43a93192b118d40c62e392
Ben Folkard <bsf509@york.ac.uk>
2025-12-05 16:53:42 +0000

    Realised that my code was unnecesarily in the with statement for longer than it had to be

--- 2025/day_2/python_solution.py
+++ 2025/day_2/python_solution.py
@@ -7,13 +7,13 @@ def day_2a_gift_shop_id_checker(filename: str) -> None:
     sum_ids = 0
     with open(filename, "r") as f:
         id_ranges = f.readline().split(",")
-        for id_range in id_ranges:
-            id_min, id_max = id_range.split("-")
-            for ID in range(int(id_min), int(id_max)+1):
-                ID_str = str(ID)
-                midpoint = len(ID_str)//2
-                if ID_str[:midpoint] == ID_str[midpoint:]:
-                    sum_ids += ID
+    for id_range in id_ranges:
+        id_min, id_max = id_range.split("-")
+        for ID in range(int(id_min), int(id_max)+1):
+            ID_str = str(ID)
+            midpoint = len(ID_str)//2
+            if ID_str[:midpoint] == ID_str[midpoint:]:
+                sum_ids += ID
     print(sum_ids)
 
 
@@ -23,25 +23,25 @@ def day_2b_gift_shop_id_checker(filename: str) -> None:
     sum_ids = 0
     with open(filename, "r") as f:
         id_ranges = f.readline().split(",")
-        for id_range in id_ranges:
-            id_min, id_max = id_range.split("-")
-            for ID in range(int(id_min), int(id_max)+1):
-                ID_str = str(ID)
-                ID_len = len(ID_str)
-                if ID_len == 2:
-                    if ID_str[0] == ID_str[1]:
-                        sum_ids += ID
-                else:
-                    # As soon as it finds the the ID is made up of a repeating sequence it stops looking
-                    for factor in range(2, ID_len+1):
-                        if ID_len % factor == 0:
-                            chunks = array_split(list(ID_str), factor)
-                            for i in range(len(chunks)-1):
-                                if not array_equal(chunks[i], chunks[i+1]):
-                                    break
-                            else:
-                                sum_ids += ID
+    for id_range in id_ranges:
+        id_min, id_max = id_range.split("-")
+        for ID in range(int(id_min), int(id_max)+1):
+            ID_str = str(ID)
+            ID_len = len(ID_str)
+            if ID_len == 2:
+                if ID_str[0] == ID_str[1]:
+                    sum_ids += ID
+            else:
+                # As soon as it finds the the ID is made up of a repeating sequence it stops looking
+                for factor in range(2, ID_len+1):
+                    if ID_len % factor == 0:
+                        chunks = array_split(list(ID_str), factor)
+                        for i in range(len(chunks)-1):
+                            if not array_equal(chunks[i], chunks[i+1]):
                                 break
+                        else:
+                            sum_ids += ID
+                            break
     print(sum_ids)
 
 
